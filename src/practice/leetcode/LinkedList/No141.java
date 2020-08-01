1c8a19e60472b268646952e4947b058cbab43d7c
tengtengwa <1511900943@qq.com>
2020-08-01 10:18:10 +0800

    141环形链表回顾，双指针和哈希表解法

--- src/practice/leetcode/LinkedList/No141.java
+++ src/practice/leetcode/LinkedList/No141.java
@@ -1,5 +1,8 @@
 package practice.leetcode.LinkedList;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class No141 {
     public static void main(String[] args) {
         Solution141 s = new Solution141();
@@ -11,17 +14,41 @@ public class No141 {
 }
 
 class Solution141 {
+    /**
+     * 题目：环形链表，判断链表是否成环
+     * 解法一：双指针
+     * 时间：O(N)，空间O(1)
+     */
     public boolean hasCycle(ListNode head) {
-        boolean isCycle = false;
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode p = head, q = p.next;
+        while (p != q) {        //注意，退出循环的条件是两指针相等，所以退出while循环后返回true
+            //先对快指针判空，再移动两个指针，如果快指针不为空，那么之前的节点都不会为空，因此不需要判断慢指针
+            if (q == null || q.next == null) {
+                return false;
+            }
+            p = p.next;
+            q = q.next.next;
+        }
+        return true;
+    }
+
+    /**
+     * 解法二：哈希表
+     * 使用哈希表保存所有节点的引用，遇到相同的引用则表明链表成环，否则将当前节点的引用加入哈希表中
+     */
+/*    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
         while (head != null) {
-            if (head.val == 947963599) {
-                isCycle = true;
-                break;
+            if (set.contains(head)) {
+                return true;
             } else {
-                head.val = 947963599;
+                set.add(head);
             }
             head = head.next;
         }
-        return isCycle;
-    }
+        return false;
+    }*/
 }
