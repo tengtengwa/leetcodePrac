c1276b074bdcbeadb445e9e1d82588a6d823eaf3
tengtengwa <1511900943@qq.com>
2020-08-05 16:32:11 +0800

    152乘积最大子数组，300最长上升子序列

---
+++ src/practice/leetcode/DP/No152.java
@@ -0,0 +1,36 @@
+package practice.leetcode.DP;
+
+public class No152 {
+    public static void main(String[] args) {
+        Solution152 s = new Solution152();
+        s.maxProduct(new int[]{-2, 0, -3});
+    }
+}
+
+class Solution152 {
+    /**
+     * 题目：乘积最大的连续子数组，数组中可能会出现负数
+     *
+     * 遍历数组时计算当前最大值，不断更新
+     * 令imax为当前最大值，则当前最大值为 imax = max(imax * nums[i], nums[i])
+     * 由于存在负数，那么会导致最大的变最小的，最小的变最大的。因此还需要维护当前最小值imin，imin = min(imin * nums[i], nums[i])
+     * 当负数出现时则imax与imin进行交换再进行下一步计算
+     * 时间复杂度：O(n)，空间O(1)
+     */
+    public int maxProduct(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int imax = 1, imin = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                int tem = imax;
+                imax = imin;
+                imin = tem;
+            }
+            //注意！！！这里如果到当前元素的连续乘积imax * nums[i]较小，imax取得就是当前元素nums[i]；imin也是对应的
+            imax = Math.max(nums[i], imax * nums[i]);
+            imin = Math.min(nums[i], imin * nums[i]);
+            max = Math.max(max, imax);
+        }
+        return max;
+    }
+}
---
+++ src/practice/leetcode/DP/No300.java
@@ -0,0 +1,80 @@
+package practice.leetcode.DP;
+
+import java.util.Arrays;
+
+public class No300 {
+    public static void main(String[] args) {
+        Solution300 s = new Solution300();
+        s.lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18, 4, 8, 6, 12});
+    }
+}
+
+class Solution300 {
+
+    /**
+     * 题目：求最长上升子序列（严格递增），也就是说可以不连续
+     * 解法一：动态规划，有点暴力，状态转移公式：dp[i] = Max(dp[j])+1，(0=<j<i,num[i]>num[j])
+     * 时间：O(N^2)，空间：O(N)
+     */
+/*    public int lengthOfLIS(int[] nums) {
+        int len = nums.length;
+        if (len < 2) {
+            return len;
+        }
+
+        int[] dp = new int[len];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < i; j++) {   //内循环求[0,i)中子序列的最长上升长度
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < len; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }*/
+
+
+    /**
+     * 解法二：贪心+二分的思想，一次遍历，遍历过程中根据情况进行二分
+     * 贪心的思想：创建一个数组用于维护一个上升序列，在遍历的过程中，遇到比数组末尾元素还大的就直接加到后面；否则在数组中通过二分查找
+     * 第一个比它大的元素，将其替换，也就是
+     */
+    public int lengthOfLIS(int[] nums) {
+        int len = nums.length;
+        if (len <= 1) {
+            return len;
+        }
+
+        int[] arr = new int[nums.length];   //arr[i] 表示长度为 i + 1 的所有上升子序列的结尾的最小值。
+        arr[0] = nums[0];
+        int end = 0;    //arr数组中末尾元素，此位置以前的元素都是严格递增的
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > arr[end]) {
+                end++;
+                arr[end] = nums[i];
+            } else {
+                //当前元素比arr末尾元素小，通过二分查找到第一个比它大的元素，将其替换。替换时已有元素长度不变，所以end不会+1
+                int left = 0, right = end;
+                while (left < right) {
+                    int mid = left + ((right - left) >>> 1);    //这里要留意，移位运算优先级小于逻辑运算符，必须加括号
+                    if (arr[mid] < nums[i]) {
+                        left = mid + 1;
+                    } else {
+                        right = mid;
+                    }
+                }
+                arr[left] = nums[i];        //跳出循环后left==right，这个位置的元素就是第一个比当前元素大的元素
+            }
+        }
+        return end + 1;     //+1即为最长上升子序列长度
+    }
+
+}
