c031307f06b79a373c268d20ce9991d55a3550ff
tengtengwa <1511900943@qq.com>
2020-08-21 23:02:50 +0800

    二叉树的序列化和反序列化，这题看看就好，略过

---
+++ src/practice/leetcode/Tree/No297.java
@@ -0,0 +1,56 @@
+package practice.leetcode.Tree;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class No297 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(5);
+
+        Codec s = new Codec();
+        s.deserialize(s.serialize(root));
+    }
+}
+
+class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        serialize(root, sb);
+        return sb.toString();
+    }
+
+    private void serialize(TreeNode root, StringBuilder sb) {
+        if (root == null) {
+            sb.append("null,");
+            return;
+        }
+        sb.append(root.val + ",");
+        serialize(root.left, sb);
+        serialize(root.right, sb);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] strs = data.split(",");
+        return deserialize(new LinkedList<>(Arrays.asList(strs)));
+    }
+
+    private TreeNode deserialize(LinkedList<String> list) {
+        if (list.get(0).equals("null")) {
+            list.remove(0);
+            return null;
+        }
+        TreeNode root = new TreeNode(Integer.parseInt(list.get(0)));
+        list.remove(0);
+        root.left = deserialize(list);
+        root.right = deserialize(list);
+        return root;
+    }
+}
